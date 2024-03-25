bc2418463b559f23a63b79c229d7a0e5de646dcb
coryhenn <coryhenn34@gmail.com>
2024-03-25 16:17:32 -0600

    Added initial files

---
+++ Mil_Fix.py
@@ -0,0 +1,19 @@
+from pymilvus import MilvusClient
+
+# Define your Milvus connection parameters
+CLUSTER_ENDPOINT = "http://localhost:19530"
+TOKEN = "YOUR_CLUSTER_TOKEN"  # If required
+
+# Set up a Milvus client
+client = MilvusClient(
+    uri=CLUSTER_ENDPOINT,
+    token=TOKEN
+)
+
+# List all collections
+collections = client.list_collections()
+
+# Print the list of collections
+print("Collections in the database:")
+for collection in collections:
+    print(collection)  # Print the collection object itself
