0ee6948eefda92b3900af9e4d6ec7eb24986e584
coryhenn <coryhenn34@gmail.com>
2024-03-28 14:10:43 -0600

    Updated scMilvus.py

--- scMilvus.py
+++ scMilvus.py
@@ -1,25 +1,35 @@
 from pymilvus import MilvusClient
 import numpy as np
 import pandas as pd
+from scipy.sparse import csr_matrix
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
 CLUSTER_ENDPOINT = "http://localhost:19530"
 TOKEN = "YOUR_CLUSTER_TOKEN"
 
-# 1. Set up a Milvus client
-client = MilvusClient(
-    uri=CLUSTER_ENDPOINT,
-    token=TOKEN
-)
+# Set up a Milvus client
+client = MilvusClient(uri=CLUSTER_ENDPOINT, token=TOKEN)
 
+# Read data from CSV file
 data = pd.read_csv('output_matrix.csv', delimiter=',')
 
 print(data.head)
 
-# Extract gene names (column headers) as metadata
-#gene_names = list(data.columns[1:])
+data_values = data.iloc[:, 1:].values
 
-# Extract gene expression values for the second row (excluding the first column)
-gene_values = data.iloc[1, 1:].tolist()
+sc = StandardScaler()
+sc_data = sc.fit_transform(data_values)
+
+pca = PCA(n_components=0.85)
+pca_data = pca.fit_transform(sc_data)
+
+
+# Exclude the first column (cell identifiers)
+#matrix_values = pca_data[:, 1:]
+
+# Extract gene expression values
+gene_values = pca_data[:, :].tolist()
 
 # Define the id value
 id_value = 0
@@ -36,6 +46,6 @@ print(result)
 # 2. Create a collection in quick setup mode
 # client.create_collection(
 
-# collection_name="PoolAscRNAseq",
+# collection_name="Exp1scRNAseq",
 # dimension=32286
 # )
